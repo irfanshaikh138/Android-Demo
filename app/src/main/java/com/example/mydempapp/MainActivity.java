e741f66820dcce86cede2e792d9be0fe5c33259a
irfanshaikh138 <irfan.shaikh@synerzip.com>
2019-10-17 18:01:14 +0530

    Activity Life Cycle Test.

--- app/src/main/java/com/example/mydempapp/MainActivity.java
+++ app/src/main/java/com/example/mydempapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mydempapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -11,42 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("demo","onCreate was called");
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("demo","onResume was called");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("demo","onStart was called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("demo","onPause was called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("demo","onStop was called");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("demo","onRestart was called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("demo","onDestroy was called");
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        // Executed when app orientation changes
+        super.onConfigurationChanged(newConfig);
+        Log.d("demo","orientation changed!!"+newConfig.orientation);
+        if(newConfig.orientation==Configuration.ORIENTATION_PORTRAIT)
+        {
+            Log.d("demo","welcome to portrait mode");
+        }
+        else if(newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+            Log.d("demo","welcome to landscape mode");
+        }
     }
 }

114e6a6f91d5e8cd4e5a49bec1829b3748687aef
irfanshaikh138 <irfan.shaikh@synerzip.com>
2019-10-18 16:04:10 +0530

    Activity Life Cycle Test.

--- app/src/main/java/com/example/mydempapp/MainActivity.java
+++ app/src/main/java/com/example/mydempapp/MainActivity.java
@@ -10,36 +10,30 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     Button button;
     Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       button=findViewById(R.id.button);
-        button2=findViewById(R.id.button2);
-        button2.setOnClickListener(this);
-       button.setOnClickListener(this);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new AllButtonsInActivity());
+        button.setOnClickListener(new AllButtonsInActivity());
     }
 
-    // here the View tells which button is clicked
- public void doSomething(View v){
-        if(v.getId()==R.id.button){
-        Log.d("demo","Button1 was Clicked");
-    } else if(v.getId()==R.id.button2){
-            Log.d("demo","Button2 was Clicked");
-        }
-    }
+    class AllButtonsInActivity implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.button) {
+                Log.d("demo", "Button One was clicked : reply by OnClickListener");
+            } else if (v.getId() == R.id.button2) {
+                Log.d("demo", "Button Two was Clicked : reply by OnClickListener");
+            }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.button){
-            Log.d("demo","Button One was clicked : reply by OnClickListener");
-        } else if(v.getId()==R.id.button2){
-            Log.d("demo","Button Two was Clicked : reply by OnClickListener");
         }
-
     }
 }

561d8382cae698fbd49d41c4a2885aa468acd89f
irfanshaikh138 <irfan.shaikh@synerzip.com>
2019-10-22 12:00:37 +0530

    Explicit Inten.

--- app/src/main/java/com/example/mydempapp/MainActivity.java
+++ app/src/main/java/com/example/mydempapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.mydempapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b= (Button) findViewById(R.id.button);
+    }
+    public void doSomething(View v){
+        // First way
+//        Intent i=new Intent(this, Main2Activity.class);
+        // Second way
+        Intent i=new Intent();
+        i.setClassName("com.example.mydempapp","com.example.mydempapp.Main2Activity");
+        startActivity(i);
     }
 }

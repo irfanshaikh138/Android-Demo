0a8f1667293fbbb1d70287a36970077d071a4df8
irfanshaikh138 <irfan.shaikh@synerzip.com>
2019-10-22 16:01:31 +0530

    Implicit Intent Practice. Implemented some example like sending mail, send image etc.

--- app/src/main/java/com/example/mydempapp/MainActivity.java
+++ app/src/main/java/com/example/mydempapp/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +21,45 @@ public class MainActivity extends AppCompatActivity {
         b= (Button) findViewById(R.id.button);
     }
     public void doSomething(View v){
-        // First way
+        Intent intent=null ,chooser=null;
+        if (v.getId()==R.id.button){
+            // First way
 //        Intent i=new Intent(this, Main2Activity.class);
-        // Second way
-        Intent i=new Intent();
-        i.setClassName("com.example.mydempapp","com.example.mydempapp.Main2Activity");
-        startActivity(i);
+            // Second way
+            intent=new Intent();
+            intent.setClassName("com.example.mydempapp","com.example.mydempapp.Main2Activity");
+            startActivity(intent);
+        }
+        if (v.getId()==R.id.launchMap){
+            intent=new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:19.076,72.8777"));
+           chooser = Intent.createChooser(intent,"Launch Maps");
+
+            startActivity(chooser);
+        }
+
+        if (v.getId()==R.id.sendImage){
+            Uri imageUri=Uri.parse("android.resource://slidenerd.vivz/drawable"+R.drawable.ic_launcher_foreground);
+            intent=new Intent(Intent.ACTION_SEND);
+            intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_STREAM,imageUri);
+            intent.putExtra(Intent.EXTRA_TEXT,"Hey, I have attached the image");
+            chooser = Intent.createChooser(intent,"Send Image");
+
+            startActivity(chooser);
+        }
+
+        if (v.getId()==R.id.sendEmail){
+            intent=new Intent(Intent.ACTION_SEND);
+            intent.setData(Uri.parse("mailto:"));
+            String[] to={"irfanshkh52@gmail.com","iamirfan321@gmail.com"};
+            intent.putExtra(Intent.EXTRA_EMAIL,to);
+            intent.putExtra(Intent.EXTRA_SUBJECT,"HI!!, This was sent from my app");
+            intent.putExtra(Intent.EXTRA_TEXT,"Hi, How are u , This is my first Email From My app");
+            intent.setType("message/rfc822");
+            chooser = Intent.createChooser(intent,"Send Email");
+            startActivity(chooser);
+        }
+
     }
 }
